2ee50c5ea725d998ea811067e01e65f52a6fffb7
egorBatarin <egorbatarin@yandex.ru>
2021-05-10 19:03:37 +0300

    added debug print

--- Neuro/ConsoleApplication4.cpp
+++ Neuro/ConsoleApplication4.cpp
@@ -154,19 +154,27 @@ void neuronet()
 
 	COMPILING: g++ main.cpp Data/data.cpp -std=c++2a
 	*/
+
 	auto z2 = (data.w2 * data.x) + data.b2;
-	//std::cout << data.b2 << z2 << std::endl;
 	matrix::Matrix<> a2(z2.getLines(), z2.getColumns());
 	std::transform(z2.begin(), z2.end(), a2.begin(), [](double elem) -> double { return (1.0 / (1.0 + std::exp(-1 * elem))); });
-	//std::cout << "a2: " << a2 << std::endl;
 	auto z3 = data.w3*a2 + data.b3;
 	matrix::Matrix<> a3(z3.getLines(), z3.getColumns());
 	std::transform(z3.begin(), z3.end(), a3.begin(), [](double elem) -> double { return (1.0 / (1.0 + std::exp(-1 * elem))); });
-
 	auto result = max_index(a3);
-	//std::cout << "a3: " << a3 << std::endl;
-	//std::cout << "result: " << result << std::endl;
-	//std::cout << data.w3 * data.b2; //example of printing multiplications of matrixes
+
+	//std::cout << "x: " << data.x << std::endl;
+	//std::cout << "b2: " << data.b2 << std::endl;
+	//std::cout << "w2*x: "<< data.w2 * data.x << std::endl;
+	//std::cout << "z2: " << z2 << std::endl;
+	//std::cout << "a2: " << a2 << std::endl;
+	//std::cout << "w3: " << data.w3 << std::endl;
+	//std::cout << "b2: " << data.b2 << std::endl;
+	//std::cout << data.w3 * data.b2 << std::endl;
+    //std::cout << "z3: " << z3 << std::endl;
+	std::cout << "a3: " << a3 << std::endl;
+	std::cout << "result: " << result << std::endl;
+
 	std::ofstream fout("result.txt"); //
 	fout << result; // Р·Р°РїРёСЃСЊ СЃС‚СЂРѕРєРё РІ С„Р°Р№Р»
 	fout.close(); // Р·Р°РєСЂС‹РІР°РµРј С„Р°Р№Р»
